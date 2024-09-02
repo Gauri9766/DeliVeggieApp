6917d6306e5b2d90e3b11aa88e63497f7e63b7bf
Gauri9766 <gaurishines@gmail.com>
2024-09-02 23:01:17 +0530

    Add files via upload

---
+++ BackEnd/MessageModels/Messages.cs
@@ -0,0 +1,49 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MessageModels
+{
+    public class Messages
+    {
+        public string Text { get; set; }
+
+    }
+    //class to hols the request type
+    public class Request
+    {
+        public RequestType RequestType { get; set; }
+        public string Id { get; set; }
+        public string CorrelationId { get; set; }
+
+
+    }
+    public enum RequestType
+    {
+        GetAll,
+        GetById
+    }
+    public class Response
+    {
+        public string CorrelationId { get; set; }
+        public List<ProductResponse> ProductsList { get; set; }
+
+    }
+
+    public class Product
+    {
+        [BsonId]
+        public ObjectId Id { get; set; } // This property maps to the MongoDB _id field
+        public string Name { get; set; }
+        public string ProductId { get; set; }
+        public string EntryDate { get; set; }
+        public string PriceForToday { get; set; }
+    }
+    public class ProductResponse
+    {
+        public string Name { get; set; }
+        public string ProductId { get; set; }
+        public string EntryDate { get; set; }
+        public string PriceForToday { get; set; }
+    }
+
+}
